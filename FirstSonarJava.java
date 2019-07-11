9104033b83d59fab1e16f702bf32286c59c4a60d
saishiva0603 <52260218+saishiva0603@users.noreply.github.com>
2019-07-11 00:56:26 -0400

    Update FirstSonarJava.java

--- FirstSonarJava.java
+++ FirstSonarJava.java
@@ -1,24 +1,10 @@
 
-import java.io.File;
-import java.io.IOException;
+public class HelloWorld {
 
-public class FirstSonarJava {
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        
+	System.out.println("Hello, World");
 
-	public static final String abc="";
-	public static String [] strings1 = {"first","second"};
-	
-	
-	public static void main(String[] args) {
-		String MayBeNull = null;
-		System.out.println("Hello SonarQube "+MayBeNull);
-		
-		//this is not a valid loop
-		for (int i = 11; i < 15; i++) { 
-			System.out.println("Loop not true  even once");
-		}
-			
 	}
-	
-
-
 }
