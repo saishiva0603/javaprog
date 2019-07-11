fad83bca6b2ec27dcd08720f34634e4abd0d3af7
saishiva0603 <52260218+saishiva0603@users.noreply.github.com>
2019-07-11 01:20:13 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
 	
-	    //this hsould work
+	    //this  work
         for(int i<10; i=10;i++){
 	
 	System.out.println("Hello, World");
