9a85a63b6c837d81996edc9adc145673f422cab9
saishiva0603 <52260218+saishiva0603@users.noreply.github.com>
2019-07-15 15:24:01 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
        
 	 //for
-        for(int i=11;i<10;i++){
+        for(int i=-1;i<10;i++){
 	
 	System.out.println("Hello, World");
 	}
